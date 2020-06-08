2cb54d0daa290d031996cfd97130e481cb422015
Felix Waage <felix.waage@sap.com>
2020-06-08 12:36:52 +0200

    http get request example

--- xpire/src/App.js
+++ xpire/src/App.js
@@ -2,25 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+    this.sendRequest = this.sendRequest.bind(this);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      product: {}
+    };
+  }
+
+  handleClick() {
+    this.sendRequest();
+  }
+
+  sendRequest() {
+    fetch("https://world.openfoodfacts.org/api/v0/product/4316268552370.json")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          product: result
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+  render() {
+    return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Send a get request:</p>
+        <button type="button" onClick={this.handleClick}>Send!</button>
+          <p>Product: {this.state.product}</p>
       </header>
     </div>
-  );
+    )
+  }
+
 }
 
 export default App;
