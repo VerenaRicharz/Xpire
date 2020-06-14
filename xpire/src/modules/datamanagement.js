19dbb74f2164a040e2df6e5b19f69b560adc8f9c
Felix Waage <felix.waage@sap.com>
2020-06-14 12:45:18 +0200

    datamanagement added

---
+++ xpire/src/modules/datamanagement.js
@@ -0,0 +1,43 @@
+var objects = [
+    {
+      name: "pizza"
+    },
+    {
+      name: "döner"
+    },
+    {
+      name: "banane"
+    },
+    {
+      name: "spiegelei"
+    },
+    {
+      name: "tomate"
+    },
+    {
+      name: "ketchup"
+    },
+    {
+      name: "apfel"
+    },
+    {
+      name: "zitrone"
+    },
+    {
+      name: "himbeere"
+    },
+  ];
+
+var addElement = (element) => {
+
+}
+
+var removeElementById = (id) => {
+
+}
+
+var getAllElements = () => {
+    return objects;
+}
+
+export {addElement,removeElementById,getAllElements}
